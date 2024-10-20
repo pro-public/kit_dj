31b780da87d300e7d59b92bc53960bf0e06f6904
Digital Node Desktop <digitalnode.ar@gmail.com>
2024-10-20 20:45:03 -0300

    Actualizo scripting..:

--- download.sh
+++ download.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-rm this.txt ; python3 nml-to-txt.py ; search_spotify.py
+echo "Chequeo de pre requisitos (librerias de python)"
+./pre_reqs.sh
+# Comprobar si existe the archivo this.txt
+if [ -f "this.txt" ]; then
+    rm this.txt
+    python3 nml-to-txt.py
+else
+    python3 nml-to-txt.py
+fi
+echo "Contenido del archivo this.txt"
+python3 read_txt.py ; sleep 2
+python3 search_spotify.py ; sleep 2
+clear ; echo "Ready at..." ; cat this.txt
